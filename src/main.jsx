8f1c15336f3531dbeec7ead57bb71bd46529b38e
marcosmorenoch <marcosmorenochaves@gmail.com>
2024-08-23 01:00:37 +0200

    pruebas

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+alert("Esto no funciona")
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
